ae2fbbb79c272d02802e181722bb39d091042269
gigaa <giga.gabatashvili@gmail.com>
2022-10-07 23:25:59 +0400

    dev doc

--- final-project/routes/secure-pages.js
+++ final-project/routes/secure-pages.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const {getUsers,getUserById,getUsersBySearch} = require('../services/customerMongo');
-const {addUser} = require('../services/usersMongo');
 
 router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+router.get('/document', function(req, res, next) {
+  res.render('document', { title: 'Document' });
+});
+router.get('/document/add', function(req, res, next) {
+  const admin = {};
+  method="POST";
+  res.render('add-document', { title: 'Add Document', buttonName:'Add Document',admin,method});
+});
 router.get('/admin', async function(req, res, next) {
   res.render('admin', { title: 'admin', data:await getUsers() });
 });
@@ -19,7 +26,7 @@ router.get('/admin/search/:field/:searchWord', async function(req, res, next) {
 router.get('/admin/add', function(req, res, next) {
   const admin = {};
   method="POST";
-  res.render('add-admin', { title: 'Add admin', buttonName:'Add',admin,method});
+  res.render('add-admin', { title: 'Add User', buttonName:'Add',admin,method});
 });
 
 
@@ -30,9 +37,7 @@ router.get('/admin/edit/:id', async function(req, res, next) {
   res.render('add-admin', { title: 'Update admin', buttonName:'Update',admin,method });
 });
 
-router.get('/about', async function(req, res, next) {
-    res.render('about', { title: 'About', admins: await getadmins() });
-});
+
 
 // user
 router.get('/signup', function(req, res, next) {
